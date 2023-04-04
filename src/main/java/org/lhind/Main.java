828c488611be6f0a233b3f40270ff8a51f1e829a
irvintopi <irvintopi@hotmail.com>
2023-04-04 18:31:20 +0200

    Edited .gitignore to include target folder.

--- src/main/java/org/lhind/Main.java
+++ src/main/java/org/lhind/Main.java
@@ -1,6 +1,6 @@
 package org.lhind;
 
-import java.sql.Date;
+
 import org.lhind.model.entity.Customer;
 import org.lhind.model.entity.Employee;
 import org.lhind.model.entity.Office;
@@ -20,6 +20,18 @@ public class Main {
         boolean exists1 = repository.exists(1002);
         System.out.println("Employee with given id exists: " + exists1);
 
+        OrderRepository orders = new OrderRepository();
+        List<Order> ords = orders.findAll();
+        System.out.println("\nAll orders\n");
+        for (Order o : ords) {
+            System.out.println(o.toString());
+        }
+        CustomerRepository customers = new CustomerRepository();
+        List<Customer> custs = customers.findAll();
+        System.out.println("\nAll customers\n");
+        for (Customer c : custs) {
+            System.out.println(c.toString());
 
+        }
     }
 }
